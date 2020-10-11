83fd9e4cbacc987089a5047f66f075e75693a226
ajstocchetti <ajstocchetti@gmail.com>
2020-10-11 12:53:15 -0500

    write to timestream

--- climate-processor/index.js
+++ climate-processor/index.js
@@ -3,25 +3,41 @@
 const AWS = require('aws-sdk');
 const timestreamwrite = new AWS.TimestreamWrite();
 
+const { TS_TABLE_NAME, TS_DB_NAME } = process.env;
+
 exports.handler = async function(event, context) {
-    const { topic, payload, thingname, timestamp } = event;
-}
+  const { payload, thingname, timestamp } = event;
 
+  const measures = ['tempf', 'humidity'];
+  const records = measures.filter(measure => payload.hasOwnProperty(measure))
+    .map(measure => ({
+      MeasureName: measure,
+      MeasureValue: payload[measure],
+    }));
 
-
-function wrie() {
+  if (!records.length) {
+    console.log('No measures provided!');
+    return;
+  }
     
-    const params = {
-        DatabaseName: '',
-        TableName: '',
-        Records: [{
-            MeasureName: '',
-            MeasureValue: '',
-            Time: '',
-            Dimensions: [
-                {Name: '', Value: ''},
-            ],
-        }],
-    };
-    return await timestreamwrite.writeRecords(params).promise();
+  const params = {
+    DatabaseName: TS_DB_NAME,
+    TableName: TS_TABLE_NAME, 
+    Records: records,
+    CommonAttributes: {
+      Dimensions: {
+        Name: 'device', Value: thingname,
+        Name: 'sensor', Value: payload.sensor,
+      },
+      Time: timestamp,
+    },
+  };
+  try {
+    const res = await timestreamwrite.writeRecords(params).promise();
+    console.log(res);
+  } catch(err) {
+    console.log('Error writing to Timestream');
+    console.log(err);
+    throw err;
+  }
 }
